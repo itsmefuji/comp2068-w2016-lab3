475a3d9d6804fb1ea00d7d4693ad094c0547553b
itsmefuji <yujif.gc@gmail.com>
2016-02-04 16:32:08 -0500

    added routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express on Azure' });
+  res.render('index', { title: 'the Home Page' });
+});
+
+/* GET About Me page. */
+router.get('/', function(req, res, next) {
+  res.render('about', { title: 'About Me' });
+});
+
+/* GET Contact Me page */
+router.get('/', function(req, res, next) {
+  res.render('contact', { title: 'Contact Me' });
+});
+
+/* GET Projects page */
+router.get('/', function(req, res, next) {
+  res.render('projects', { title: 'Projects' });
+});
+
+/* GET Service page. */
+router.get('/', function(req, res, next) {
+  res.render('services', { title: 'Services' });
 });
 
 
